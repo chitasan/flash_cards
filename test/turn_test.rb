f1fd6ae4e32a8a690f475e5f019b24f46f07bb63
Chi Tran <chidtran@gmail.com>
2019-01-25 20:55:29 -0700

    Add Turn test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,28 +1,67 @@
-class Turn
-  attr_reader :string, :card
 
-  def initialize(string, card)
-    @string = string
-    @card = card
+require 'minitest/autorun' #start from directory you're running it from
+require 'minitest/pride'
+require './lib/turn' #start from this file loc
+require './lib/card'
+
+
+class TurnTest < Minitest::Test
+
+  def test_it_exists
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_instance_of Turn, turn
   end
 
-  def guess
-    @string
+  def test_it_returns_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expected = "Juneau"
+    actual = turn.guess
+
+    assert_equal "Juneau", turn.guess
   end
 
-  def correct? # refactor without else?
-    if @string == @card.answer
-      true
-    else
-      false
-    end
+  def test_it_is_a_correct_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expected = true
+    actual = turn.correct?
+
+    assert_equal true, turn.correct?
   end
 
-  def feedback
-    if self.correct?
-      "Correct!"
-    else
-      "Incorrect."
-    end
+  def test_it_gives_feedback_for_correct_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expected = "Correct!"
+    actual = turn.feedback
+
+    assert_equal "Correct!", turn.feedback
   end
+
+ def test_it_is_an_incorrect_guess
+   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+   turn = Turn.new("Anchorage", card)
+
+   expected = false
+   actual = turn.correct?
+
+   assert_equal false, turn.correct?
+ end
+
+ def test_it_gives_feedback_for_incorrect_guess
+   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+   turn = Turn.new("Anchorage", card)
+
+   expected = "Incorrect."
+   actual = turn.feedback
+
+   assert_equal "Incorrect.", turn.feedback
+ end
+
 end
